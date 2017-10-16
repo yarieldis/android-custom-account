ed9b654714bfb8991ca42040ace0269190fdb8b5
yarieldis <yarieldis@aleph.engineering>
2017-10-16 16:14:14 -0400

    Define custom account files and structure

---
+++ app/src/main/java/com/yarieldis/customaccount/CustomAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.yarieldis.customaccount
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_account)
+    }
+}
---
+++ app/src/main/java/com/yarieldis/customaccount/service/CustomAccountService.kt
@@ -0,0 +1,11 @@
+package com.yarieldis.customaccount.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class CustomAccountService : Service() {
+    override fun onBind(p0: Intent?): IBinder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
